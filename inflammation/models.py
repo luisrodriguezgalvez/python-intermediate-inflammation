9395f815f8c0f31b333d9b36eef42dd91fe114db
luisrodriguezgalvez <luis.rodriguez@deltares.nl>
2023-03-22 12:17:37 +0100

    add standard deviation function

--- inflammation/models.py
+++ inflammation/models.py
@@ -48,6 +48,14 @@ def daily_min(data):
     return np.min(data, axis=0)
 
 
+def daily_std(data):
+    """Calculate the standard deviation of a 2D inflammation data array for each day.
+
+    :param data: A 2D data array with inflammation data (each row contains measurements
+    for a single patient across all days).
+    :returns: An array of standard deviation values of measurements for each day.
+    """
+    return np.std(data, axis=0)
 
 
 def patient_normalise(data):
